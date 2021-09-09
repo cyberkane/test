e447a64696b92ecd76b462e36b0338f13826d5a9
Chebakov Vitalii <cyberkane@live.ru>
2021-09-09 21:11:57 +0500

    123

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-print('Hello world!')
-print('Hello world!')
-print('Hello world!')
-print('Hello world!')
+import tensorflow as tf
+import pandas
+import prepairing_data
+from plotting import plot_from_file
+
+group_names,group_data = prepairing_data.data_from_file()
+
+data_1, data_2 = list(group_data[2]), list(group_data[5])
+name_1, name_2 = group_names[2], group_names[5]
+
+data_1 = [int(i) for i in data_1]
+sorted(data_1)
+
+#plot_from_file(data_1, data_2, name_1, name_2)
+
+
+print(data_1)
---
+++ prepairing_data.py
@@ -0,0 +1,25 @@
+import pyparsing as ps
+import numpy as np
+from pyparsing import *
+
+file_name = 'taxi_test.xls'
+
+def data_from_file():
+    
+    with open(file_name, encoding='utf-8') as file:
+        data = file.read().split('\n')
+    
+    data_list = []
+
+    for i in range (len(data) - 1):
+        csv_value = OneOrMore(CharsNotIn(','))
+        csv_value.ignore(',')
+        data_string = list(csv_value.parseString(data[i]))
+        data_list.append(data_string)
+
+    data_list_names = data_list[0]
+    data_list.remove(data_list[0])
+
+    data_list = np.transpose(data_list)
+
+    return(data_list_names, data_list)
