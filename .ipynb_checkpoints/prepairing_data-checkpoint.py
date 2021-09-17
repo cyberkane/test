c1e5458f65e11eee31e140459497f5cf0b31f063
Chebakov Vitalii <cyberkane@live.ru>
2021-09-17 14:13:37 +0500

    ++ add

---
+++ .ipynb_checkpoints/prepairing_data-checkpoint.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+file_name = 'taxi_formated.xlsx'
+
+def testing_data():
+    data = pd.read_excel(file_name)
+    test_data = data.sample(frac = 0.3).reset_index(drop = True) # 30% исходных данных для подготовки тестового набора
+    data = pd.concat([data, test_data], ignore_index=True).drop_duplicates(keep = False).reset_index(drop = True) # оставшиеся данные идут для подготовки данных для обучения
+    return(data, test_data)
+
+
+#запись наборов данных в отдельные файлы
+'''
+learn_data, test_data = testing_data()
+
+learn_data.to_excel('learn_data.xlsx')
+test_data.to_excel('test_data.xlsx')
+'''
+
+data_for_learning = 'learn_data.xlsx' #подготовленный файл learn_data.xlsx через prepairing_data.py
+data_for_testing = 'test_data.xlsx'   #подготовленный файл test_data.xlsx через prepairing_data.py
+
+learn_data = pd.read_excel(data_for_learning) #обучающий датафрейм
+test_data = pd.read_excel(data_for_testing) #тестовый датафрейм
--- main.py
+++ main.py
@@ -1,14 +1,12 @@
+import matplotlib.pyplot as plt
 from prepairing_data import *
 
-learn_data, test_data = testing_data()
-
 def plot_from_file(data1, data2, group_name1, group_name2):
-    plt.plot(data1, data2, label= str(group_name1 + '/' + group_name2))  # Plot some data on the (implicit) axes.
+    plt.plot(data1, data2, label= str(group_name1 + '/' + group_name2))
     plt.xlabel(str(group_name1))
     plt.ylabel(str(group_name2))
     plt.title("Simple Plot")
     plt.legend()
     plt.show()
 
-print(test_data)
-print(learn_data)
+plot_from_file(predict, predicted, 'Час суток', 'Продолжительность')
